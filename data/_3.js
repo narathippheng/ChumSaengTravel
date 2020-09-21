35f76f439adb32cc1cc568994352abb80d4058f9
narathippheng <narathipphengphit@gmail.com>
2020-09-21 20:10:32 +0700

    ChumSaengTravel

---
+++ data/_3.js
@@ -0,0 +1,19 @@
+var json__3 = {
+"type": "FeatureCollection",
+"name": "_3",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "OID_": 0.0, "Name": "สถานีรถไฟชุมแสง" }, "geometry": { "type": "Point", "coordinates": [ 100.310059685000041, 15.894664668000079 ] } },
+{ "type": "Feature", "properties": { "OID_": 0.0, "Name": "ตลาดเก่าร้อยปีเมืองชุมแสง" }, "geometry": { "type": "Point", "coordinates": [ 100.308968986000068, 15.894978584000054 ] } },
+{ "type": "Feature", "properties": { "OID_": 0.0, "Name": "ชุมแสงแกลอรี่" }, "geometry": { "type": "Point", "coordinates": [ 100.308721268000042, 15.89566700900002 ] } },
+{ "type": "Feature", "properties": { "OID_": 0.0, "Name": "สะพานหิรัญนฤมิต" }, "geometry": { "type": "Point", "coordinates": [ 100.307405892000077, 15.894454774000053 ] } },
+{ "type": "Feature", "properties": { "OID_": 0.0, "Name": "อนุเสาวรีย์สมเด็จพระเจ้าตากสินมหาราช" }, "geometry": { "type": "Point", "coordinates": [ 100.308240585000064, 15.894992033000053 ] } },
+{ "type": "Feature", "properties": { "OID_": 0.0, "Name": "พระบรมธาตเกยไชย วัดเกยไชยเหนือ" }, "geometry": { "type": "Point", "coordinates": [ 100.263193508000086, 15.869219710000042 ] } },
+{ "type": "Feature", "properties": { "OID_": 0.0, "Name": "จุดบรรจบแม่น้ำยมน่าน" }, "geometry": { "type": "Point", "coordinates": [ 100.263995932000043, 15.870606976000035 ] } },
+{ "type": "Feature", "properties": { "OID_": 0.0, "Name": "พิธภัณฑ์ราชาจระเข้ยักษ์" }, "geometry": { "type": "Point", "coordinates": [ 100.262311498000088, 15.868546957000033 ] } },
+{ "type": "Feature", "properties": { "OID_": 0.0, "Name": "พาทีโอ บูติค  เฮาส์" }, "geometry": { "type": "Point", "coordinates": [ 100.254950047000065, 15.85754450700006 ] } },
+{ "type": "Feature", "properties": { "OID_": 0.0, "Name": "วัดเกยไชยใต้" }, "geometry": { "type": "Point", "coordinates": [ 100.258426227000072, 15.866969738000019 ] } },
+{ "type": "Feature", "properties": { "OID_": 0.0, "Name": "วัดท่าจันทร์วิปัสนา" }, "geometry": { "type": "Point", "coordinates": [ 100.250989574000059, 15.860387487000025 ] } },
+{ "type": "Feature", "properties": { "OID_": 0.0, "Name": "โรงเรียนชาวนา บ้านเกยไชยใต้" }, "geometry": { "type": "Point", "coordinates": [ 100.25784202100003, 15.860103751000056 ] } }
+]
+}
